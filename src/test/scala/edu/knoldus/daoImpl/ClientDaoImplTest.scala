3d0f0c3f815616569e599d13c86ba6987580d4dd
ashish1269 <ashish1269@gmail.com>
2017-02-01 21:16:20 +0530

    Testing done! All test 'units' working fine in IDE. 8 tests failed in terminal because of referential intigrity violation and primary key duplication.

--- src/test/scala/edu/knoldus/daoImpl/ClientDaoImplTest.scala
+++ src/test/scala/edu/knoldus/daoImpl/ClientDaoImplTest.scala
@@ -1,28 +1,51 @@
 package edu.knoldus.daoImpl
 
-import edu.knoldus.model.Client
-import org.scalatest.FunSuite
+import edu.knoldus.model.{Client, Department, Project}
+import org.scalatest.{BeforeAndAfterAll, FunSuite}
 
 /**
   * Created by ashish on 2/1/17.
   */
-class ClientDaoImplTest extends FunSuite{
+class ClientDaoImplTest extends FunSuite with BeforeAndAfterAll{
+
+  override def beforeAll(): Unit = {
+
+    val department = Department(1269, "Pantry")
+    val departmentDaoImpl = new DepartmentDaoImpl
+    departmentDaoImpl.createDepartment(department)
+
+    //id 9012 is already in client
+    val project = Project(1023, 1269, "Employee Assessment System", 9012)
+    val projectDaoImpl = new ProjectDaoImpl
+    projectDaoImpl.createProject(project)
+
+  }
+
+  override def afterAll(): Unit = {
+
+    val projectDaoImpl = new ProjectDaoImpl
+    projectDaoImpl.removeProject(1023)
+
+    val departmentDaoImpl = new DepartmentDaoImpl
+    departmentDaoImpl.removeDepartment(1269)
+
+  }
 
   test("test createClient() ") {
-    val client = Client(543903L,3904, "Bombay Rockers", "Mumbai")
+    val client = Client(543903L,1023, "Bombay Rockers", "Mumbai")
     val clientDaoImpl = new ClientDaoImpl
     assert(clientDaoImpl.createClient(client))
   }
 
   test("test updateClient()") {
-    val client = Client(543903L,3904, "Kolkata Bloomers", "Kolkata")
+    val client = Client(543903L,1023, "Kolkata Bloomers", "Kolkata")
     val clientDaoImpl = new ClientDaoImpl
     assert(clientDaoImpl.updateClient(client))
   }
 
   test("test getClient() ") {
     val clientId = 543903L
-    val client = Client(543903L,3904, "Kolkata Bloomers", "Kolkata")
+    val client = Client(543903L,1023, "Kolkata Bloomers", "Kolkata")
     val clientDaoImpl = new ClientDaoImpl
     assert(clientDaoImpl.getClient(clientId) == client)
   }
--- src/test/scala/edu/knoldus/mysqlworkder/MysqlWorkerTest.scala
+++ src/test/scala/edu/knoldus/mysqlworkder/MysqlWorkerTest.scala
@@ -14,6 +14,11 @@ class MysqlWorkerTest extends FunSuite{
     assert(MysqlWorker.pushData(query) == 1)
   }
 
+  test("Update in department") {
+    val query : String = s"update department set name = 'Marketing' where id=67512"
+    assert(MysqlWorker.updateData(query) == 1)
+  }
+
   test("Delete from department") {
     val query : String = s"delete from department where id = 67512"
     assert(MysqlWorker.deleteData(query) == 1)
@@ -24,14 +29,46 @@ class MysqlWorkerTest extends FunSuite{
     assert(MysqlWorker.pushData(query) == 1)
   }
 
+  test("Update in employee") {
+    val query : String = s"update employee set address = 'Ranchi' where id=67512"
+    assert(MysqlWorker.updateData(query) == 1)
+  }
+
+  test("Delete from employee") {
+    val query : String = s"delete from employee where id = 67512"
+    assert(MysqlWorker.deleteData(query) == 1)
+  }
+
   test("Insert in client") {
     val query : String = s"insert into client values(612, 123, 'IBM', 'Delhi');"
     assert(MysqlWorker.pushData(query) == 1)
   }
 
+  test("Update in client") {
+    val query : String = s"update client set address = 'Ranchi' where id=612"
+    assert(MysqlWorker.updateData(query) == 1)
+  }
+
+  test("Delete from client") {
+    val query : String = s"delete from client where id=612"
+    assert(MysqlWorker.deleteData(query) == 1)
+  }
+
   test("Insert in project") {
-    val query : String = s"insert into project values(3904, 67512, 'Scala Geek', 612);"
+    //id 67519 already exist in department
+    //id 9012 already exist in client
+    val query : String = s"insert into project values(3905, 67519, 'Scala Geek', 9012);"
     assert(MysqlWorker.pushData(query) == 1)
+  }
+
+  test("Update in project") {
+    val query : String = s"update project set name = 'Scala Worms' where id=3905"
+    assert(MysqlWorker.updateData(query) == 1)
+  }
+
+  test("Delete from project") {
+    val query : String = s"delete from project where id = 3905"
+    assert(MysqlWorker.deleteData(query) == 1)
   }
 
 }
